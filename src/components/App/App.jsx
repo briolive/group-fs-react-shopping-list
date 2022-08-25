c1a1682861f410d76d7a38353d39ad3f8f313766
Brianna Oliveira <mystikgab@gmail.com>
2022-08-25 10:34:15 -0500

    create a simple wireframe to start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {useEffect, useState} from 'react'; // Destructuring
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
@@ -7,8 +8,14 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <Header />
+            <Header/>
             <main>
+            <br/>
+            <form onSubmit={addItem}>
+                Item<input id="item" type="text"></input>
+                <input id="quanity" type="text">Quantity</input>
+                <input id="unit" type="text">Unit</input>
+            </form>
                 <p>Under Construction...</p>
             </main>
         </div>

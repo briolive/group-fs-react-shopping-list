66c704e9b68e00feaa08a16c86779381ad473588
Brianna Oliveira <mystikgab@gmail.com>
2022-08-25 10:04:20 -0500

    add existing items to the database and update database.sql

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+## TO DO: 
+
+-[x] install react 
+-[ ] make sure you hve both servers started
+-[ ] set up a database 
+-[ ] 
--- database.sql
+++ database.sql
@@ -1,2 +1,15 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+
+CREATE TABLE groceries (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80) NOT NULL,
+	quantity NUMERIC NOT NULL,
+	unit VARCHAR(20)
+);
+
+INSERT INTO groceries (name, quantity, unit)
+VALUES ('Andouille sausages', 4, 'links'),
+('Heirloom tomatoes', 2, 'tomatoes'),
+('Honey Bunches of Oats Cereal', 1, 'box');

f5dc9cadb055411ccb8273e0cb00ce35793d38c5
Brianna Oliveira <mystikgab@gmail.com>
2022-08-25 10:43:27 -0500

    create item name, quantity, and unit variables with useState

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,15 +6,25 @@ import './App.css';
 
 
 function App() {
+
+    const [itemName, setItemName] = useState('');
+    const [itemQuantity, setItemQuantity] = useState(0);
+    const [itemUnit, setItemUnit] = useState('');
+
+
+    const addItem = (event) => {
+        event.preventDefault();
+    }
+
     return (
         <div className="App">
             <Header/>
             <main>
             <br/>
             <form onSubmit={addItem}>
-                Item<input id="item" type="text"></input>
-                <input id="quanity" type="text">Quantity</input>
-                <input id="unit" type="text">Unit</input>
+                Item<input id="item" type="text"/>
+               Quantity <input id="quanity" type="text"/>
+               Unit <input id="unit" type="text"/>
             </form>
                 <p>Under Construction...</p>
             </main>
